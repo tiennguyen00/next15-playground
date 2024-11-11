487e7c9680503e1c1bde7adee54ba167b671fa0a
Tien Nguyen <nhtiendev@gmail.com>
2024-11-11 16:45:39 +0700

    chore

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,33 +1,43 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({
   searchParams,
 }: {
-  params: Promise<{ slug: string }>;
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+  searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-  const posts = await client.fetch(STARTUPS_QUERY);
-  console.log(JSON.stringify(posts));
+  const params = { search: query || null };
+
+  // const session = await auth();
+
+  // console.log(session?.id);
+
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
       <section className="pink_container">
         <h1 className="heading">
-          Pitch your startup, Connect with entrepreneurs
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
         </h1>
+
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitches, and Get Notices in Virtual
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
         </p>
-        <SearchForm query={query as string} />
+
+        <SearchForm query={query} />
       </section>
+
       <section className="section_container">
         <p className="text-30-semibold">
-          {query ? `Search result for ${query}` : "All startups"}
+          {query ? `Search results for "${query}"` : "All Startups"}
         </p>
+
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
             posts.map((post: StartupTypeCard) => (
@@ -38,6 +48,8 @@ export default async function Home({
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 }
